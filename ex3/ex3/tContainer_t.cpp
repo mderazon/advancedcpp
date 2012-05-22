379dd8ad7caf29f849c186d562ca792599424f53
mderazon <mderazon@hotmail.com>
2012-05-22 11:16:27 +0000

    adding tcontainer class
    git-svn-id: http://advancedcpp.googlecode.com/svn/trunk@29 1f8e3cb1-2606-9ffd-e45a-b7facdcfa802

---
+++ ex3/ex3/tContainer_t.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "tContainer_t.h"
+
+
+tContainer_t::tContainer_t(void)
+{
+}
+
+
+tContainer_t::~tContainer_t(void)
+{
+}
---
+++ ex3/ex3/tContainer_t.h
@@ -0,0 +1,14 @@
+#pragma once
+
+template<typename T>
+class tContainer_t
+{
+public:
+	tContainer_t(void);
+	~tContainer_t(void);
+
+	isEmpty()
+};
+
+private:
+	
