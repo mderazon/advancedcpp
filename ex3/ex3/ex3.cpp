cdb3952787a84734b15a11a145a5a77346713b73
mderazon <mderazon@hotmail.com>
2012-05-22 20:17:35 +0000

    fixing bugs
    git-svn-id: http://advancedcpp.googlecode.com/svn/trunk@32 1f8e3cb1-2606-9ffd-e45a-b7facdcfa802

--- ex3/ex3/ex3.cpp
+++ ex3/ex3/ex3.cpp
@@ -24,12 +24,20 @@ public:
 template<typename T, typename R>
 std::string tContainer_t<T, R>::Print()
 {
+	int i = 0;
 	std::ostringstream s;
-	s << "[";
-	for(auto it = Container.begin(); it != Container.end(); it++)
+	s << "[ ";
+	typename R::const_iterator it;
+	for(it = Container.begin(); it != Container.end(); it++)
 	{
 		s << (**it);
+		if (i != Container.size()-1)
+		{
+			s << ", ";
+		}
+		i++;
 	}
+	s << " ]";
 	return s.str();
 }
 
@@ -42,13 +50,12 @@ void tContainer_t<T, R>::Insert( T* element )
 template<typename T, typename R>
 int tContainer_t<T, R>::Size()
 {
-	return R.size();
+	return Container.size();
 }
 
 template<typename T, typename R>
 T tContainer_t<T, R>::GetLast()
 {
-	//R<T*>::const_iterator it = Container.end();
 	return Container.end();
 }
 
@@ -61,7 +68,7 @@ T tContainer_t<T, R>::GetFirst()
 template<typename T, typename R>
 bool tContainer_t<T, R>::IsEmpty()
 {
-	R.empty()
+	Container.empty()
 }
 
 //CTOR
