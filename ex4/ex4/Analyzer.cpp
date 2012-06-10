0cfc87ee0b5621f6d529e51bea04b204f53ca9bc
mderazon <mderazon@hotmail.com>
2012-06-10 09:43:07 +0000

    git-svn-id: http://advancedcpp.googlecode.com/svn/trunk@45 1f8e3cb1-2606-9ffd-e45a-b7facdcfa802

--- ex4/ex4/Analyzer.cpp
+++ ex4/ex4/Analyzer.cpp
@@ -5,12 +5,14 @@ char const* types_[] = { "char", "short", "int", "long", "float", "double", "voi
 char const* control_[] = { "if", "else", "for" , "while", "function"};
 char const* operators_[] = {"++",  "--",  "==",  "->" ,  "=",  "+",  "-",  "*",  "&",  "<<",  ">>"};
 char const* modifiers_[] = {"class", "private", "public", "const", "virtual"};
+char const* enclosures_[] = { "(", ")", "{", "}", "<", ">", "[", "]" };
 
 std::set<std::string> reservedWords;
-std::set<std::string> types;
-std::set<std::string> control;
-std::set<std::string> operators;
-std::set<std::string> modifiers;
+std::set<std::string> types(std::begin(types_),std::end(types_));
+std::set<std::string> control(std::begin(control_),std::end(control_));
+std::set<std::string> operators(std::begin(operators_),std::end(operators_));
+std::set<std::string> modifiers(std::begin(modifiers_),std::end(modifiers_));
+std::set<std::string> enclosures(std::begin(enclosures_),std::end(enclosures_));
 
 
 
@@ -18,10 +20,10 @@ std::set<std::string> modifiers;
 Analyzer::Analyzer()
 {
 	reservedWords.insert(std::begin(types_),std::end(types_));
-	types.insert(std::begin(types_),std::end(types_));
-	control.insert(std::begin(control_),std::end(control_));
-	operators.insert(std::begin(operators_),std::end(operators_));
-	modifiers.insert(std::begin(modifiers_),std::end(modifiers_));
+	reservedWords.insert(std::begin(control_),std::end(control_));
+	reservedWords.insert(std::begin(operators_),std::end(operators_));
+	reservedWords.insert(std::begin(modifiers_),std::end(modifiers_));
+	reservedWords.insert(std::begin(enclosures_),std::end(enclosures_));
 }
 
 
